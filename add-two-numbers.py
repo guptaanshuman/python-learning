43d75cd967a350d164d1080211213ee5664347a8
Anshuman Gupta <anshumangupta@mastersindia.co>
2018-12-13 14:57:14 +0530

    add files

---
+++ add-two-numbers.py
@@ -0,0 +1,8 @@
+# Add two numbers		# starting with # in Python programming is a comment.
+
+num1 = 3				# num1 is a variable. You can store a value in a variable. Here, 3 is stored in this variable.
+num2 = 4				# num2 is a variable. You can store a value in a variable. Here, 4 is stored in this variable.
+
+sum = num1 + num2		# The variables num1 and num2 are added using + operator. The result of addition is then stored in another variable sum
+
+print(sum)				# The print() function prints the output to the screen.
